6e40c87fff221e00b17387335053f3b426df9808
Trenk_lods <Alekssherel@gmail.com>
2022-06-29 23:49:06 +0300

    app chat (dialogue window)

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const http =require('http').createServer(app)
+const io=require('socket.io')(http)
+
+const port = 3000
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/dialog/index.html')
+})
+app.use(express.static(__dirname+'/assets'))
+
+io.on('connection',(socket)=>{
+  socket.on('chat message',(data)=>{
+    io.emit('chat message',{
+      message:data.message,
+      name:data.name
+    })
+  })
+})
+
+http.listen(port,()=>{
+  console.log('run server')
+})
